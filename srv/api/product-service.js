f19c71156ac9356dee0a401e0503f826e94d3576
I320393 <madhusudan.sahu@sap.com>
2022-08-26 10:06:03 +0530

    hana db added.

--- srv/api/product-service.js
+++ srv/api/product-service.js
@@ -2,26 +2,11 @@ const ProductValidator = require("../validator/product-validator");
 module.exports = class ProductService extends cds.ApplicationService{
     init(){
         
-        this.before('READ', 'Product', req=>{
+        this.before('CREATE', 'Product', req=>{
             if(!ProductValidator.validateProduct(req.data)){
                 req.reject(400, 'mrp is greater than 1000' );
             }
-        })
-        this.on('CREATE', "Product", async(req, next)=>{
-          const products =   await SELECT.from(Product);
-         let  flag = false;
-         let result ;
-          products.forEach(product => {
-            if(product.name ===req.data.name){
-                result =  product;
-                flag = true;                
-            }
-          });
-          if(!flag)
-         return  next();
-         else return result;
-        })
-
+        });
 
         super.init();
     }
